ed7e200a63eed7409e7d37317a96dee040d3232c
Justin Mathew <jmathew2111@yahoo.com>
2020-06-09 20:39:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,16 +1,21 @@
 def create_an_empty_array
   
+  []
+  
 end
 
 def create_an_array
+  [1,2,3,4]
   
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array,element)
+   array = ["Tesla,Ford,GM"] 
+   array << element = "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
+  
   
 end
 
