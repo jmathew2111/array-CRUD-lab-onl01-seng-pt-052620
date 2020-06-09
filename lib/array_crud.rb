18a4902fd0ef06739c55c929114bdb49f50c97f3
Justin Mathew <jmathew2111@yahoo.com>
2020-06-09 20:47:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,8 +37,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = 
-  array 
+  array = ["am"]
+  array [0]
 end
 
 def retrieve_last_element_from_array(array)
